9cc98949be9e1a35c7fc147514aeae3275f6a9a8
miskonm <miskonm@gmail.com>
2017-01-26 11:34:04 +0300

    Initial Commit

---
+++ SoftTecoTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoftTecoTest
+//
+//  Created by Nikita Misko on 26.01.17.
+//  Copyright © 2017 Nikita Misko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
